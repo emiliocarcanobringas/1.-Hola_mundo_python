6220b00598eff5f1c85653e211924f3d9c6b4d6e
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 19:26:54 -0600

    Add files via upload

---
+++ 1.-Hola_mundo_python.py
@@ -0,0 +1,5 @@
+# Este programa muestra como imprimir un mensaje en pantalla.
+print("Estoy orgulloso de pertenecer a Uninaciones y estudiar en la UNAM")
+print("Mi nombre es Emilio Carcaño Bringas")
+
+#Este programa fue escrito por Emilio Carcaño Bringas
